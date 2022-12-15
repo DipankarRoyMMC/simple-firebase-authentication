b9308aa3647377de3bb5d607b247266cc07f0150
DIPANKAR <you@example.com>
2022-12-15 20:38:46 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
 
-  const handleSignInGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const handleSignInWithGoogle = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
-        console.log(error);
+        console.error('error', error);
       })
-
   }
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      })
+  }
+
 
   return (
     <div className="App">
       <h1>Simple Firebase Authentication.</h1>
-      <button onClick={handleSignInGoogle}>Sign in Google</button>
+      {/* conditional rendering  */}
+      {user.email ? <button onClick={handleSingOut}>Sing Out</button> :
+        <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
+
+      }
+
+      <h2>Name : {user.displayName}</h2>
+      <p>User Email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
